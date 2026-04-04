0b79b161c1883af92a8bb40d34eb9678b8620367
BSX LABS <156119164+BSXLAbS2025@users.noreply.github.com>
2026-04-04 17:51:06 +0300

    Create mac_changer_eth.sh

---
+++ evasion/mac_changer_eth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Evasion: MAC Address Spoofer
+INTERFACE="eth0" # Измени на wlan0 если ты на Wi-Fi
+NEW_MAC="00:$(printf '%02x:%02x:%02x:%02x:%02x' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))"
+
+echo "[*] Changing MAC for $INTERFACE to $NEW_MAC..."
+sudo ip link set dev $INTERFACE down
+sudo ip link set dev $INTERFACE address $NEW_MAC
+sudo ip link set dev $INTERFACE up
+echo "[!] MAC Address changed successfully."
